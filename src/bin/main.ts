9d091f4dbc306cda0ce36afd9e74931010e87e67
devgui <guihermepereiradv@gmail.com>
2025-01-14 11:23:12 -0300

    feat(src/bin): get version from package.json

--- src/bin/main.ts
+++ src/bin/main.ts
@@ -1,10 +1,11 @@
 import { initCommands } from '@/commands/command';
 import { program } from 'commander';
+import packageJson from '../../package.json' with { type: "json" };
 
 const main = async () => {
     program
         .name('fuze')
-        .version('0.0.1')
+        .version(packageJson.version)
         .description('A CLI tool to generate base structure for hono projects')
         .usage('<command> [options]')
         .helpOption('-h, --help', 'Display help for command')
