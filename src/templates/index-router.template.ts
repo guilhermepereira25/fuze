4a886c21d7f1bc0a7adae6f68c2df195e946a69b
devgui <guihermepereiradv@gmail.com>
2025-01-17 15:02:40 -0300

    fix(src/templates): better templates

--- src/templates/index-router.template.ts
+++ src/templates/index-router.template.ts
@@ -1,6 +1,8 @@
-import { Hono } from 'hono';
-import * as routes from './${resourceName}.router.ts';
+import { Hono } from "hono";
+import * as routes from "./${resourceName}.router.ts";
+import * as handlers from "./${resourceName}.handler.ts"
 
-const router = new Hono();
+const router = new Hono()
+    .router(routes.findOne, handlers.findOne);
 
 export default router;
--- src/templates/router.template.ts
+++ src/templates/router.template.ts
@@ -1,6 +1,5 @@
 import { Hono } from "hono";
-import * as handlers from 
 
-export const getOne = new Hono();
+export const getOne = new Hono()
 
 export const create = new Hono();
